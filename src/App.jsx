eedea5149dfead5efa55b9cf8c385e4c338cdd60
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-02-02 21:27:12 +0530

    loding added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,25 @@
-import { useState, useEffect, useCallback } from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import Card from './Card';
 import './app.css';
 import Single from './Single';
+import ErrorPage from './ErrorPage';
 
 const App = () => {
 
       const [movies, setMovies] = useState([]);
       const [pageCount, setpageCount] = useState(1);
+      const [isLoading, setIsLoading] = useState(false);
 
       const prevPage = () => {
-            if (pageCount <= 1) {
-                  setpageCount(1)
-                  return
-            }
+            if (pageCount <= 1) { setpageCount(1); return }
             setpageCount(prevPageCount => prevPageCount - 1)
       }
-      const nextvPage = () => {
-            setpageCount(pageCount + 1)
-      }
 
-      const pageRouter = createBrowserRouter([
-            {
-                  path: "/",
-                  element: <Card movies={movies} nextvPage={nextvPage} prevPage={prevPage} pageCount={pageCount} />
-            },
-            {
-                  path: "/single/:id",
-                  element: <Single />,
-            }
-      ])
+      const nextvPage = () => { setpageCount(pageCount + 1) }
 
       useEffect(() => {
-            const url = `https://api.themoviedb.org/3/movie/popular?page=${pageCount}`;
+            const url = `${import.meta.env.VITE_BASE_URL}/movie/upcoming?page=${pageCount}`;
             const options = {
                   method: 'GET',
                   headers: {
@@ -42,20 +29,37 @@ const App = () => {
             };
             const fetchMovieAPI = async () => {
                   try {
+                        setIsLoading(true);
                         const response = await (await fetch(url, options)).json();
                         setMovies(response.results);
                   } catch (e) {
                         console.log("Error fetching weather " + e);
+                  } finally {
+                        setIsLoading(false);
                   }
             }
             fetchMovieAPI();
       }, [pageCount])
 
+      const allPropsForCard = { isLoading, movies, nextvPage, prevPage, pageCount }
+
+      const pageRouter = createBrowserRouter([
+            {
+                  path: "/",
+                  element: <Card {...allPropsForCard} />,
+                  errorElement: <ErrorPage />,
+            },
+            {
+                  path: "/single/:id",
+                  element: <Single isLoading={isLoading} setIsLoading={setIsLoading} />,
+            }
+      ])
+
       return (<>
             <main className="w-svh h-svh overflow-y-auto bg-slate-800 grid md:grid-cols-1/3.5 grid-cols-1">
                   <aside className="sidebar bg-slate-950 h-full overflow-y-auto px-2 hidden md:block">
                         <div className="h-20 flex items-center justify-center">
-                              <a href="/">
+                              <a href='/'>
                                     <h1 className="text-slate-100 font-syne text-4xl uppercase font-bold text-center">Cine Search</h1>
                               </a>
                         </div>
@@ -65,8 +69,10 @@ const App = () => {
                               <div className="search-box min-w-[60%] h-10 border rounded-md bg-slate-800 border-slate-700">
                                     <input className="text-slate-300 font-syne text-sm font-semibold uppercase w-full h-full bg-transparent outline-none p-2" type="text" placeholder="Search Movies" />
                               </div>
-                              <a href='/'>
-                                    <button className="text-slate-100"><i className="fa-brands fa-facebook"></i></button>
+                              <a href="/">
+                                    <button className="text-slate-100">
+                                          <i className="fa-brands fa-facebook"></i>
+                                    </button>
                               </a>
                         </header>
                         <div className="main-content p-4 overflow-y-auto">
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,27 +1,35 @@
-import { memo } from "react"
+import { Link } from "react-router-dom"
 
-const Card = ({ movies, prevPage, nextvPage, pageCount }) => {
+const Card = ({ movies, prevPage, nextvPage, pageCount, isLoading }) => {
       return (<>
-            <div className="w-full align-middle grid grid-cols-2 md:grid-cols-6 gap-4">
-                  {movies.map(movie => (
-                        <div key={movie.id} className="card max-w-md mx-auto bg-slate-900 rounded overflow-hidden shadow-lg">
-                              <a href={`/single/${movie.id}`}>
-                                    <img className="w-full aspect-2/3 object-cover" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.original_title} />
-                              </a>
-                              <div className="p-4">
-                                    <h2 className="text-slate-100 text-base font-semibold mb-2 font-syne">{movie.original_title}</h2>
-                                    <a href={`/single/${movie.id}`} className="text-blue-600 font-syne uppercase font-semibold text-xs">Read more</a>
-                              </div>
-                        </div>
-                  ))}
 
-            </div>
-            <div className="p-4 flex justify-center items-center gap-6">
-                  {pageCount !== 1 &&
-                        <a href='#' onClick={prevPage} className='py-1 px-3 font-syne rounded-sm uppercase text-sm border border-slate-500 text-slate-100'>Prev</a>}
-                  <a href='#' onClick={nextvPage} className='py-1 px-3 font-syne rounded-sm uppercase text-sm border border-slate-500 text-slate-100'>next</a>
-            </div>
+            {isLoading ?
+                  <div className="flex w-full h-[80svh] justify-center items-center">
+                        <h6 className="font-syne text-xl font-semibold text-slate-400">Loading...</h6>
+                  </div>
+                  :
+                  <div>
+                        <div className="w-full align-middle grid grid-cols-2 md:grid-cols-6 gap-4">
+                              {movies.map(movie => (
+                                    <div key={movie.id} className="card max-w-md mx-auto bg-slate-900 rounded overflow-hidden shadow-lg">
+                                          <Link to={`/single/${movie.id}`}>
+                                                <img className="w-full aspect-2/3 object-cover" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.original_title} />
+                                          </Link>
+                                          <div className="p-4">
+                                                <h2 className="text-slate-100 text-base font-semibold mb-2 font-syne">{movie.original_title}</h2>
+                                                <Link to={`/single/${movie.id}`} className="text-blue-600 font-syne uppercase font-semibold text-xs">Read more</Link>
+                                          </div>
+                                    </div>
+                              ))}
+
+                        </div>
+                        <div className="p-4 flex justify-center items-center gap-6">
+                              {pageCount !== 1 &&
+                                    <a href='#' onClick={prevPage} className='py-1 px-3 font-syne rounded-sm uppercase text-sm border border-slate-500 text-slate-100'>Prev</a>}
+                              <a href='#' onClick={nextvPage} className='py-1 px-3 font-syne rounded-sm uppercase text-sm border border-slate-500 text-slate-100'>next</a>
+                        </div>
+                  </div>}
       </>)
 }
 
-export default memo(Card)
+export default Card
