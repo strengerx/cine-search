c491e5dbf29397aefe6200606f84685c23e9a459
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-02-02 21:30:58 +0530

    Error Page Added

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 
 const ErrorPage = () => {
-      return (
-            <div>ErrorPage</div>
-      )
+      return (<>
+            <div className="flex w-full h-[80svh] justify-center items-center">
+                  <h6 className="font-syne text-xl font-semibold text-slate-400">No Content Found</h6>
+            </div>
+      </>)
 }
 
 export default ErrorPage
