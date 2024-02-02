dedbda4e4cdeb0aa6ea1c342aace7cc8c2c242da
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-02-02 22:44:27 +0530

    toggle button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Card from './Card';
 import './app.css';
 import Single from './Single';
@@ -19,7 +19,7 @@ const App = () => {
       const nextvPage = () => { setpageCount(pageCount + 1) }
 
       useEffect(() => {
-            const url = `${import.meta.env.VITE_BASE_URL}/movie/upcoming?page=${pageCount}`;
+            const url = `${import.meta.env.VITE_BASE_URL}/movie/top_rated?page=${pageCount}`;
             const options = {
                   method: 'GET',
                   headers: {
@@ -43,40 +43,32 @@ const App = () => {
 
       const allPropsForCard = { isLoading, movies, nextvPage, prevPage, pageCount }
 
-      const pageRouter = createBrowserRouter([
-            {
-                  path: "/",
-                  element: <Card {...allPropsForCard} />,
-                  errorElement: <ErrorPage />,
-            },
-            {
-                  path: "/single/:id",
-                  element: <Single isLoading={isLoading} setIsLoading={setIsLoading} />,
-            }
-      ])
-
       return (<>
             <main className="w-svh h-svh overflow-y-auto bg-slate-800 grid md:grid-cols-1/3.5 grid-cols-1">
                   <aside className="sidebar bg-slate-950 h-full overflow-y-auto px-2 hidden md:block">
                         <div className="h-20 flex items-center justify-center">
-                              <a href='/'>
+                              <Link to='/'>
                                     <h1 className="text-slate-100 font-syne text-4xl uppercase font-bold text-center">Cine Search</h1>
-                              </a>
+                              </Link>
                         </div>
                   </aside>
                   <section className="content h-auto overflow-y-auto">
                         <header className="bg-slate-900 h-20 flex justify-between items-center px-4 sticky top-0 right-0">
-                              <div className="search-box min-w-[60%] h-10 border rounded-md bg-slate-800 border-slate-700">
+                              <Link className='sm:hidden' to={"/"} >
+                                    <h2 className="text-slate-100 font-syne text-2xl uppercase font-bold text-center">Cine</h2></Link>
+                              <div className="hidden sm:block search-box min-w-[60%] h-10 border rounded-md bg-slate-800 border-slate-700">
                                     <input className="text-slate-300 font-syne text-sm font-semibold uppercase w-full h-full bg-transparent outline-none p-2" type="text" placeholder="Search Movies" />
                               </div>
-                              <a href="/">
-                                    <button className="text-slate-100">
-                                          <i className="fa-brands fa-facebook"></i>
-                                    </button>
-                              </a>
+                              <div className="text-slate-400 w-[30px] h-[30px] rounded-full bg-transparent border- text-xl flex justify-center items-center cursor-pointer">
+                                    <div className='w-[90%] relative h-[3px] rounded-full bg-slate-300 before:w-[85%] before:h-full before:rounded-full before:bg-slate-300 before:absolute before:top-[8px] before:content-[""] before:left-0 after:w-[85%] after:h-full after:rounded-full after:bg-slate-300 after:absolute after:top-[-8px] after:content-[""] after:left-0 before:translate-x-[-4px] after:translate-x-[-4px]' ></div>
+                              </div>
                         </header>
                         <div className="main-content p-4 overflow-y-auto">
-                              <RouterProvider router={pageRouter} />
+                              <Routes>
+                                    <Route path='/' element={<Card {...allPropsForCard} />} />
+                                    <Route path='/single/:id' element={<Single isLoading={isLoading} setIsLoading={setIsLoading} />} />
+                                    <Route path='*' element={<ErrorPage />} />
+                              </Routes>
                         </div>
 
                   </section>
