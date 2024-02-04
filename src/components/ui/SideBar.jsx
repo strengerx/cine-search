a4c609c7bf42690bc60d90960b8dcc3824153ccc
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-02-05 01:24:02 +0530

    sidebar changed

--- src/components/ui/SideBar.jsx
+++ src/components/ui/SideBar.jsx
@@ -3,10 +3,10 @@ const SideBar = ({ toggle, setToggle }) => {
 
       const asideToggleCss = { right: "0" };
       const playableCatagory = [
-            { path: "top_rated", name: "Top Rated" },
             { path: "popular", name: "popular" },
-            { path: "now_playing", name: "now playing" },
             { path: "upcoming", name: "upcoming" },
+            { path: "top_rated", name: "Top Rated" },
+            { path: "now_playing", name: "now playing" },
       ]
       return (<>
             <aside style={toggle ? asideToggleCss : {}} className="sidebar transition-all duration-500 bg-slate-950 h-full overflow-y-auto px-2 sm:static fixed right-[-350px] top-0 z-50 w-min(90%,350px) sm:w-auto ease-in-out">
@@ -14,16 +14,23 @@ const SideBar = ({ toggle, setToggle }) => {
                         <div className="sm:hidden flex justify-start w-full px-5 h-20 items-center">
                               <button onClick={() => setToggle(!toggle)} className=' text-3xl text-slate-300 capitalize font-semibold'><i className="fa-solid fa-xmark"></i></button>
                         </div>
-                        <Link className='hidden sm:block' to='/'>
-                              <h1 className="text-slate-100 font-syne text-4xl uppercase font-bold text-center">Cine Search</h1>
+                        <Link className="sm:block hidden" to='/'>
+                              <h1 className="text-slate-100 font-syne text-4xl uppercase font-bold">Cine Search</h1>
                         </Link>
                   </div>
-                  <div className="w-full min-h-[80svh] flex flex-col items-end sm:items-start justify-start gap-4 text-slate-300 px-5 py-5">
+                  <Link className="sm:hidden" to='/'>
+                              <h1 className="text-slate-100 font-syne text-2xl px-5 text-start uppercase font-bold">Cine Search</h1>
+                  </Link>
+                  <div className="w-full min-h-[80svh] flex flex-col items-start justify-center gap-6 font-syne text-slate-300 px-5 py-5">
+                        <div className="" ><Link onClick={() => setToggle(false)} className='capitalize font-thin text-xl' to={`/`} >Home</Link></div>
                         {playableCatagory && playableCatagory.map((cate, key) => (
-                              <div key={key}><Link onClick={() => setToggle(false)} className='uppercase font-semibold text-base' to={`/movie/${cate.path}`} >{cate.name}</Link></div>
+                              <div className="" key={key}><Link onClick={() => setToggle(false)} className='capitalize font-thin text-xl' to={`/movie/${cate.path}`} >{cate.name}</Link></div>
                         ))}
                   </div>
-            </aside>
+                  <div className="social-section px-5 py-5">
+                        <h6 className="text-slate-500 font-syne capitalize">All Rights Reserved | © cine 2024</h6>
+                  </div>
+            </aside >
       </>)
 }
 
